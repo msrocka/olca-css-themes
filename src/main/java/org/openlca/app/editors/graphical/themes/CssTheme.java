bba2a0670765405f8fd2fa40e00aabdd23d59070
msrocka <michael.srocka@gmail.com>
2021-05-28 18:40:07 +0200

    color mapping

--- src/main/java/org/openlca/app/editors/graphical/themes/CssTheme.java
+++ src/main/java/org/openlca/app/editors/graphical/themes/CssTheme.java
@@ -5,7 +5,9 @@ import java.io.File;
 import com.helger.css.reader.CSSReader;
 import com.helger.css.reader.CSSReaderSettings;
 
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
+import org.openlca.app.util.Colors;
 
 public class CssTheme {
 
@@ -29,10 +31,37 @@ public class CssTheme {
     if (css == null)
       return null;
 
-    var theme =  new CssTheme(file.getName());
-
+    var theme = new CssTheme(file.getName());
 
     return theme;
+  }
+
+  private static Color color(String s) {
+    if (s == null)
+      return Colors.black();
+    if (s.startsWith("#"))
+      return Colors.fromHex(s);
+    return switch (s.trim().toLowerCase()) {
+    case "white" -> Colors.systemColor(SWT.COLOR_WHITE);
+    case "black" -> Colors.systemColor(SWT.COLOR_BLACK);
+    case "darkred" -> Colors.systemColor(SWT.COLOR_DARK_RED);
+    case "darkgreen" -> Colors.systemColor(SWT.COLOR_DARK_GREEN);
+    case "darkblue" -> Colors.systemColor(SWT.COLOR_DARK_BLUE);
+    case "darkmagenta" -> Colors.systemColor(SWT.COLOR_DARK_MAGENTA);
+    case "darkyellow" -> Colors.systemColor(SWT.COLOR_DARK_YELLOW);
+    case "darkcyan" -> Colors.systemColor(SWT.COLOR_DARK_CYAN);
+    case "gray" -> Colors.systemColor(SWT.COLOR_GRAY);
+    case "grey" -> Colors.systemColor(SWT.COLOR_GRAY);
+    case "darkgray" -> Colors.systemColor(SWT.COLOR_DARK_GRAY);
+    case "darkgrey" -> Colors.systemColor(SWT.COLOR_DARK_GRAY);
+    case "red" -> Colors.systemColor(SWT.COLOR_RED);
+    case "green" -> Colors.systemColor(SWT.COLOR_GREEN);
+    case "yellow" -> Colors.systemColor(SWT.COLOR_YELLOW);
+    case "blue" -> Colors.systemColor(SWT.COLOR_BLUE);
+    case "magenta" -> Colors.systemColor(SWT.COLOR_MAGENTA);
+    case "cyan" -> Colors.systemColor(SWT.COLOR_CYAN);
+    default -> Colors.black();
+    };
   }
 
   enum BoxType {
@@ -53,6 +82,13 @@ public class CssTheme {
     Color borderColor;
     Color fontColor;
     Color backgroundColor;
+
+    BoxConfig() {
+      borderWidth = 1;
+      borderColor = Colors.black();
+      fontColor = Colors.black();
+      backgroundColor = Colors.white();
+    }
   }
 
 }
